5934ab5e77526a1cfffb92899dfd70b7b49265bb
Emrys <legall.emrys@gmail.com>
2026-01-30 23:35:54 +0100

    import scripts

---
+++ Assets/Scripts/Monster/MonsterVisual.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterVisual : MonoBehaviour
+{
+    public Dictionary<int, GameObject> MaskDict = new Dictionary<int, GameObject>();
+    public Dictionary<int, GameObject> MeshDict = new Dictionary<int, GameObject>();
+}
