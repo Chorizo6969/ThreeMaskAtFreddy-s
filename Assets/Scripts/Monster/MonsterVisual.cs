20a5d8af1fd6d56da75abedbe5a057b4379d9048
Emrys <legall.emrys@gmail.com>
2026-01-31 00:28:11 +0100

    Mask prefabs start

--- Assets/Scripts/Monster/MonsterVisual.cs
+++ Assets/Scripts/Monster/MonsterVisual.cs
@@ -3,20 +3,56 @@ using UnityEngine;
 
 public class MonsterVisual : MonoBehaviour
 {
-    public Dictionary<MaskType, GameObject> MaskDict = new Dictionary<MaskType, GameObject>();
-    public Dictionary<int, GameObject> MeshDict = new Dictionary<int, GameObject>();
-
-    [SerializeField] private GameObject _currentMaskEquiped;
+    [Header("Current Meshes")]
+    [SerializeField] private GameObject _currentMaskEquipped;
     [SerializeField] private GameObject _currentMonsterPose;
 
+    [Header("Meshes Lists")]
+    [SerializeField] private List<MaskPair> maskPairs;
+    [SerializeField] private List<PosMeshPair> posMeshPairs;
 
-    public void ChangeMaskMesh(MaskType _newMaskType)
+    private Dictionary<MaskType, GameObject> MaskDict;
+    private Dictionary<int, GameObject> MeshDict;
+
+    private void Awake()
     {
-        _currentMaskEquiped = MaskDict[_newMaskType];
+        InitDictionaries();
     }
 
-    public void ChangeMonsterMesh(int _newMonsterPos)
+    void InitDictionaries()
     {
-        _currentMonsterPose = MeshDict[_newMonsterPos];
+        MaskDict = new Dictionary<MaskType, GameObject>();
+        foreach (var pair in maskPairs)
+            MaskDict[pair.maskType] = pair.maskMesh;
+
+        MeshDict = new Dictionary<int, GameObject>();
+        foreach (var pair in posMeshPairs)
+            MeshDict[pair.pos] = pair.PosMesh;
+    }
+
+    public void ChangeMaskMesh(MaskType newMaskType)
+    {
+        if (MaskDict.TryGetValue(newMaskType, out var mesh))
+            _currentMaskEquipped = mesh;
+    }
+
+    public void ChangeMonsterMesh(int newMonsterPos)
+    {
+        if (MeshDict.TryGetValue(newMonsterPos, out var mesh))
+            _currentMonsterPose = mesh;
+    }
+
+    [System.Serializable]
+    public class MaskPair
+    {
+        public MaskType maskType;
+        public GameObject maskMesh;
+    }
+
+    [System.Serializable]
+    public class PosMeshPair
+    {
+        public int pos;
+        public GameObject PosMesh;
     }
 }
