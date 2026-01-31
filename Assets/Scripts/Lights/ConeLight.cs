af9576e2729d67f745bdeed5054145cfbcda62f4
JElouann <eljouenne@gmail.com>
2026-01-31 09:31:41 +0100

    Cone light trop stylée

---
+++ Assets/Scripts/Lights/ConeLight.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ConeLight : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        Debug.Log("Yo!");
+        //this.transform.GetChild(0).gameObject.SetActive(false);
+    }
+}
