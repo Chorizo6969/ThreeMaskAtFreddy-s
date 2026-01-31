a05927d297f47936689ec0f71a40c0c3572304b1
Emrys <legall.emrys@gmail.com>
2026-01-31 13:17:41 +0100

    DebugRaycast

--- Assets/Scripts/Monster/Row/DirectionLookDetector.cs
+++ Assets/Scripts/Monster/Row/DirectionLookDetector.cs
@@ -3,4 +3,19 @@ using UnityEngine;
 public class DirectionLookDetector : MonoBehaviour
 {
     public DirectionType DirectionType;
+    public bool IsWatched = false;
+
+    [SerializeField] private Renderer _renderer;
+
+    private void Update()
+    {
+        if (!IsWatched)
+        {
+            _renderer.material.color = Color.black;
+        }
+        else
+        {
+            _renderer.material.color = Color.red;
+        }
+    }
 }
--- Assets/Scripts/Player/CameraVisionRay.cs
+++ Assets/Scripts/Player/CameraVisionRay.cs
@@ -3,24 +3,33 @@ using UnityEngine;
 public class CameraVisionRay : MonoBehaviour
 {
 
-    [SerializeField] private DirectionType lastDirectionLooked;
+    [SerializeField] private DirectionType _lastDirectionLooked;
+    [SerializeField] private DirectionLookDetector _lastComponent;
     void Update()
     {
         RaycastHit hitDirection;
-        if(Physics.Raycast(transform.position, transform.forward, out hitDirection, 7))
+        if (Physics.Raycast(transform.position, transform.forward, out hitDirection, 500, ~7))
         {
-            hitDirection.transform.gameObject.TryGetComponent<DirectionLookDetector>(out DirectionLookDetector component);
-            lastDirectionLooked = component.DirectionType;
-            MonsterMain.Instance.MonsterMovement.RemoveFromValideDirection(lastDirectionLooked);
-        }
-        else
-        {
-            MonsterMain.Instance.MonsterMovement.AddToValideDirection(lastDirectionLooked);
+            Debug.Log("JE TOUCHE le trigger");
+            if (hitDirection.transform.gameObject.TryGetComponent<DirectionLookDetector>(out DirectionLookDetector component))
+            {
+
+                component.IsWatched = true;
+                _lastDirectionLooked = component.DirectionType;
+                MonsterMain.Instance.MonsterMovement.RemoveFromValideDirection(_lastDirectionLooked);
+
+            }
+            else
+            {
+                MonsterMain.Instance.MonsterMovement.AddToValideDirection(_lastDirectionLooked);
+                if (_lastComponent != null) _lastComponent.IsWatched = false;
+            }
         }
 
         RaycastHit hitMonster;
-        if (Physics.Raycast(transform.position, transform.forward, out hitMonster, 6))
+        if (Physics.Raycast(transform.position, transform.forward, out hitMonster, 500, ~6))
         {
+            Debug.Log("JE TOUCHE le monstre");
             MonsterMain.Instance.IsWatchedByPlayer = true;
         }
         else
@@ -28,5 +37,6 @@ public class CameraVisionRay : MonoBehaviour
             MonsterMain.Instance.IsWatchedByPlayer = false;
         }
 
+        Debug.DrawRay(transform.position, transform.forward * 500, Color.red);
     }
 }
