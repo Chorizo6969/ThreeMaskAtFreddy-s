7d8aaec3e7b56b4676c61807a32c8d5688816421
JElouann <eljouenne@gmail.com>
2026-01-31 15:02:34 +0100

    mask, matéo sagouin

--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -2,22 +2,27 @@ using UnityEngine;
 
 public class PlayerAnim : MonoBehaviour
 {
-    [SerializeField] private Animator animator;
+    //[SerializeField] private Animator animator;
 
     public void PlayEquipMask(int maskIndex)
     {
-        animator.speed = 1f;
-        animator.SetFloat("MaskIndex", maskIndex);
-        animator.SetBool("IsWearing", true);
-        animator.Play("TakesMask", 0, 0f);
+        #region V1 de merde
+        //animator.speed = 1f;
+        //animator.SetFloat("MaskIndex", maskIndex);
+        //animator.SetBool("IsWearing", true);
+        //animator.Play("TakesMask", 0, 0f);
+
+        #endregion
     }
 
     public void PlayRemoveMask(int maskIndex)
     {
-        animator.SetFloat("MaskIndex", maskIndex);
-        animator.SetBool("IsWearing", false);
-        animator.Play("TakesMask", 0, 1f);
-        animator.Update(0f);
-        animator.speed = -1f;
+        #region V1 de merde
+        //animator.SetFloat("MaskIndex", maskIndex);
+        //animator.SetBool("IsWearing", false);
+        //animator.Play("TakesMask", 0, 1f);
+        //animator.Update(0f);
+        //animator.speed = -1f;
+        #endregion
     }
 }
