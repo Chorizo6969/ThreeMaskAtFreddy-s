830299a967c4028f79fa081f74d13e82009cec7c
MeguminFireFox <ambrehardy013@gmail.com>
2026-01-31 13:26:56 +0100

    Ui Radio

--- Assets/Scripts/Radio/Radio.cs
+++ Assets/Scripts/Radio/Radio.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Radio : MonoBehaviour
 {
-    [SerializeField] private float Goal = 1f;
+    [SerializeField] private float _goal = 1f;
+    [SerializeField] private Slider _slider;
     public float Current = 0f;
 
     public void Repair()
     {
         Current += 0.0003f;
+        _slider.value = Current;
 
-        if (Current >= Goal)
+        if (Current >=_goal)
         {
-            Current = Goal;
+            Current = _goal;
             //Victoire
         }
     }
